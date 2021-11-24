d2003c9e5667f79ee6f5be797927ef5782f3172b
josue1919 <josuefig19@gmail.com>
2021-11-24 11:39:12 -0600

    Informatio about develop

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,38 @@ btnActive.addEventListener("click",()=>{
     menu.classList.toggle("active");
 
 });
+
+//para el scroll suave
+/*const links = document.querySelectorAll(".header-menu li a");
+ 
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+ 
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+ 
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
+
+*/
+
+const links = document.querySelectorAll(".header-menu li a");
+ 
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+ 
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+ 
+  document.querySelector(href).scrollIntoView({
+    behavior: "smooth"
+  });
+}
